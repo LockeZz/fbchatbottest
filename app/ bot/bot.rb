e38c03933949422c8009faaeb2df2f11b768d904
LockeZz <LockeZz.PHF@gmail.com>
2021-05-04 16:53:27 +0800

    updated params

--- app/ bot/bot.rb
+++ app/ bot/bot.rb
@@ -2,9 +2,11 @@ require 'facebook/messenger'
 
 include Facebook::Messenger 
 
+SUBSCRIBED_PARAMS = ['messages']
+
 Facebook::Messenger::Subscriptions.subscribe(
     access_token: ENV["ACCESS_TOKEN"],
-    subscribed_fields: %w[feed mention name]
+    subscribed_fields: SUBSCRIBED_PARAMS
 )
 
 Facebook::Messenger::Bot.on :message do |message| 
