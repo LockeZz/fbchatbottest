82c0297acd86362171103fd0b651057b048dc158
LockeZz <LockeZz.PHF@gmail.com>
2021-05-05 16:40:26 +0800

    message option updated

--- app/ bot/bot.rb
+++ app/ bot/bot.rb
@@ -10,5 +10,23 @@ Facebook::Messenger::Subscriptions.subscribe(
 )
 
 Facebook::Messenger::Bot.on :message do |message| 
-    message.reply(text: 'Hello human!')
+    if message.text.include? "Bot"
+        message.reply(text: "Yes, I'm a sexy Robot!")
+    elsif message.text.include? "Show me something"
+        message.reply(
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: 'button',
+                    text: 'Human, do you love me?',
+                    buttons: [
+                        { type: 'postback', title: 'Yes <3', payload: 'HARMLESS'},
+                        { type: 'postback', title: 'No, you freaks', payload: 'EXTERMINATE'}
+                    ]
+                }
+            }
+        )  
+    else
+        message.reply(text: 'Hello human!')
+    end
 end
